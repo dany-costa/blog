31f104a89d86a7ce6676a46c2fb68fbde941b320
dany-costa <dany-costa@outlook.pt>
2020-06-07 11:01:40 +0100

    Commit

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -7,7 +7,7 @@ require('../params')(router);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./server/public/");
+    cb(null, "./public/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
