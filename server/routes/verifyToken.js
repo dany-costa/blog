358b4b3a195d3a2f8425baff2eda229e1aee3273
dany-costa <dany-costa@outlook.pt>
2020-07-01 21:54:01 +0100

    css fix

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -1,15 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-module.exports = function(req, res, next){
-    const token = req.header('Authorization');
-    if(!token) return res.status(400).send('Access Denied');
+module.exports = function (req, res, next) {
+  const token = req.header("Authorization");
+  if (!token)
+    return res
+      .status(400)
+      .send({ success: false, error: { message: "Access Denied" } });
 
-    try{
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.user = verified;
-        next();
-    }
-    catch (err){
-        res.status(400).send('Invalid Token');
-    }
-}
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res
+      .status(400)
+      .send({ success: false, error: { message: "Invalid Token" } });
+  }
+};
